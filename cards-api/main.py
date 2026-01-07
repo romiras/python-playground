187116ed39a474b6c54f3c03248f36b187d9c5af
Romiras <298544+romiras@users.noreply.github.com>
2026-01-07 13:01:38 +0200

    sort cards

--- cards-api/main.py
+++ cards-api/main.py
@@ -1,9 +1,17 @@
+from typing import Any
 import requests
+from functools import cmp_to_key
+
 
 class DeckManager:
     BASE_API_HOST = "https://www.deckofcardsapi.com"
-    CARDS_SUITS_ORDER = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-    RANKS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A']
+    CARDS_SUITS_ORDER = [
+        "S", # Spades
+        "C", # Clubs
+        "D", # Diamonds
+        "H", # Hearts
+    ]
+    RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A']
 
     def __init__(self):
         self.cards = []
@@ -39,28 +47,36 @@ class DeckManager:
         data = self._api_get(f"/api/deck/{self.deck_id}/draw/?count={count}")
         self.cards = data['cards']
 
-    def _card_code_to_rank(code: str) -> int:
+    def _card_code_to_rank(self, card_item: dict[str, Any]) -> int:
         """
         extract a code and convert to rank
         """
-        pass
+        code = card_item["code"]
+        value = code[0]
+        suit = code[1]
+        return (len(self.CARDS_SUITS_ORDER) - self.CARDS_SUITS_ORDER.index(suit)) * 16 + \
+                self.RANKS.index(value)
 
-    def _sort_cards():
-        sorted_cards = []
-        for suit in CARDS_SUITS_ORDER:
-            # sort cards by suit
-            # then sort cards code
-            rank = self.RANKS.index(code)
+    def _compare(self, card_item1, card_item2):
+        return self._card_code_to_rank(card_item2) - self._card_code_to_rank(card_item1)
+
+    def _sort_cards(self):
+        self.cards = sorted(self.cards, key=cmp_to_key(self._compare))
 
     def print_cards(self):
         print("Cards:")
-        print(self.cards)
+        # print cards in one line
+        for card_item in self.cards:
+            print(f"{card_item['code']}", end=" ")
+        print()
 
 def main():
     print("--Deck manager--")
     man = DeckManager()
     man.generate_new_shuffled_deck()
     man.draw_cards(15)
+    man.print_cards()
+    man._sort_cards()
     man.print_cards()
 
 if __name__ == "__main__":
